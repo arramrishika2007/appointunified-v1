ebe731cb3607ddaab6e859f8f4d56c8f0a649a5c
Arram Rishika <135079553+arramrishika2007@users.noreply.github.com>
2026-09-20 20:31:01 +0530

    Update Next.js config with experimental options

--- appointunified-v1/frontend/next.config.mjs
+++ appointunified-v1/frontend/next.config.mjs
@@ -1,4 +1,4 @@
-/**  @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -6,6 +6,9 @@ const nextConfig = {
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
     ],
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
   },
   async headers() {
     return [
